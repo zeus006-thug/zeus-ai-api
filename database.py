63afb48be3345869399c10e8ffbf2beb9103de57
zeus006-thug <harishtechtamil2020@gmail.com>
2025-08-08 22:13:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 # database.py
 
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
